53c591a82149bba358248284361d9e9d21712ad8
JingZhou <zhouruge@163.com>
2018-11-22 16:06:21 -0500

    first commit

---
+++ src/ga/sorce/ShowImage.java
@@ -0,0 +1,32 @@
+package ga.sorce;
+
+import java.awt.*;
+
+public class ShowImage extends Frame { 
+
+    
+
+    String filename; 
+
+    public ShowImage (String filename) { 
+
+        this.setSize(420,350); 
+
+        this.setVisible(true); 
+
+        this.filename = filename; 
+
+    } 
+
+     
+
+    public void paint (Graphics g) { 
+
+        Image image = this.getToolkit().getImage(filename); 
+
+        g.drawImage(image, 0, 0, this); 
+
+    } 
+
+
+}   
